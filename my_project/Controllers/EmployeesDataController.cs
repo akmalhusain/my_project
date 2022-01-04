5a55221c86e7c06b0259a804f69669c0be0729cd
akmalhusain <70431817+akmalhusain@users.noreply.github.com>
2022-01-04 17:55:16 +0530

    Add files via upload

---
+++ my_project/Controllers/EmployeesDataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace my_project.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+
+        public string[] myEmployees = { "akmal", "ajmal", "sadiya", "foziya", "srk", "aayat" };
+       
+        public string[] GetEmployees()
+        {
+            return myEmployees;
+        }
+        public string GetEmployeesByIndex( int id)
+        {
+            return myEmployees[id];
+        }
+
+    }
+}
